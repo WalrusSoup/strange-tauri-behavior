9fa73a825b2349170693c070f8bd66886c07dbc5
JL <jay.lindsley@gmail.com>
2024-04-26 15:49:14 -0700

    updating with some inline notes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::panic;
+use std::panic::PanicInfo;
 use log::info;
 use tauri::{
-    AppHandle, CloseRequestApi, CustomMenuItem, Manager, SystemTray, SystemTrayMenu, SystemTrayMenuItem, Window, WindowBuilder, WindowUrl, Wry
+    AppHandle, CloseRequestApi, CustomMenuItem, Manager, SystemTray, SystemTrayMenu, SystemTrayMenuItem, Window, WindowBuilder, WindowUrl, Wry,
 };
 use tauri_plugin_log::LogTarget;
 
+// CALLING THIS = EXIT CODE 11 - SEGFAULT
 #[tauri::command]
 async fn close_window(app_handle: AppHandle<Wry>) -> Result<(), ()> {
     info!("Going to programatically close windows - which we don't do");
     let windows = app_handle.windows();
-    for(_, window) in windows {
+    for (_, window) in windows {
         // kill the window
         window.close().unwrap();
     }
@@ -26,26 +29,34 @@ fn main() {
         .invoke_handler(tauri::generate_handler![close_window])
         .build(tauri::generate_context!())
         .expect("error while running tauri application")
-        .run(|handle, event| match event {
-            tauri::RunEvent::Ready => {
-                tauri_ready(&handle);
-                start_background_thread(&handle);
-            },
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                info!("Exit requested - intercepting");
-            },
-            tauri::RunEvent::WindowEvent { event, .. } => match event {
-                tauri::WindowEvent::Destroyed => {
-                    info!("Window destroyed - but will this be called when we crash? Should we re-open the window?");
-                },
-                tauri::WindowEvent::CloseRequested { api, .. } => {
-                    info!("Intercepting close");
-                    intercept_close_request(handle, &api);
-                },
+        .run(|handle, event| {
+            match event {
+                tauri::RunEvent::Ready => {
+                    tauri_ready(&handle);
+                    start_background_thread(&handle);
+                }
+                tauri::RunEvent::ExitRequested { api, .. } => {
+                    info!("Exit requested - intercepting");
+                }
+                tauri::RunEvent::WindowEvent { event, .. } => match event {
+                    tauri::WindowEvent::Destroyed => {
+                        info!("Window destroyed - but will this be called when we crash? Should we re-open the window?");
+                    }
+                    tauri::WindowEvent::CloseRequested { api, .. } => {
+                        info!("Intercepting close");
+                        intercept_close_request(handle, &api);
+                    }
+                    _ => {}
+                }
                 _ => {}
-            }
-            _ => {}
+            };
         });
+
+    panic::set_hook(Box::new(|info: &PanicInfo| {
+        let location = info.location().unwrap();
+        let message = info.to_string();
+        info!("Panic occurred: {} at {}", message, location);
+    }));
 }
 
 fn start_background_thread(app_handle: &AppHandle<Wry>) {
@@ -61,17 +72,18 @@ fn start_background_thread(app_handle: &AppHandle<Wry>) {
 
     let handle_two = app_handle.clone();
 
+    // LETTING THIS RUN = PANIC
     // std::thread::spawn(move || {
     //     loop {
     //         std::thread::sleep(std::time::Duration::from_secs(5));
     //         info!("CLOSING WINDOWS TO SEE WHAT HAPPENS");
     //         let window_thing = handle_two.get_window("main").unwrap().url();
     //         let windows = handle_two.windows();
-    //         for(_, window) in windows {
+    //         for (_, window) in windows {
     //             // kill the window
     //             window.close().unwrap();
     //         }
-
+    //
     //         info!("GOING TO CALL WINDOW URL NOW TO SEE WHAT HAPPENS");
     //         let window_ref = handle_two.get_window("main").unwrap();
     //         info!("GOT WINDOW REFERENCE - CHECKING URL");
@@ -91,16 +103,14 @@ fn intercept_close_request(app_handle: &AppHandle<Wry>, api: &CloseRequestApi) {
 
 fn tauri_ready(handle: &AppHandle<Wry>) {
     let main_window = WindowBuilder::new(handle, "main", WindowUrl::App("index.html".into()))
-    .title("Window CLoser Test")
-    .resizable(true)
-    .fullscreen(false)
-    .min_inner_size(375.00, 500.00)
-    .decorations(true)
-    .inner_size(1280.00, 800.00)
-    .build()
-    .expect("Error building main window.");
-
-    main_window.open_devtools();
+        .title("Window CLoser Test")
+        .resizable(true)
+        .fullscreen(false)
+        .min_inner_size(375.00, 500.00)
+        .decorations(true)
+        .inner_size(1280.00, 800.00)
+        .build()
+        .expect("Error building main window.");
 }
 
 fn create_app_tray() -> SystemTray {
